4cf1211d170755dabf0da483803401fbdb077a1a
Valerii Volkov <v.volkov@start.ru>
2024-03-18 20:48:55 +0400

    fixup

--- lua/nvim-virt-text-fn/init.lua
+++ lua/nvim-virt-text-fn/init.lua
@@ -13,28 +13,47 @@ local DEFAULT_RENDER_CONFIG = {
   virt_text_highlight = "Todo",
 }
 
+local function echo(message, highlight)
+  vim.api.nvim_echo({ { message, highlight } }, true, {})
+end
+
 local function create_virt_text_callback(opts)
   local mapping_config = opts.mapping or DEFAULT_MAPPING_CONFIG
   local render_config = vim.tbl_extend("keep", opts.render or {}, DEFAULT_RENDER_CONFIG)
   local render = render.create_render(render_config)
 
   return function()
-    local items = {}
     local win = vim.api.nvim_get_current_win()
     local bufnr = vim.api.nvim_win_get_buf(win)
-    for _, fn in ipairs(parsers.parse_functions_in_viewpoint(win)) do
-      local handler = mapping_config[fn.name]
+    vim.schedule(function()
+      local items = {}
+      for _, fn in ipairs(parsers.parse_functions_in_viewpoint(win)) do
+        local handler = mapping_config[fn.name]
 
-      local _, ret = pcall(handler, bufnr, fn.line)
-      table.insert(items, { text = ret, line = fn.line })
-    end
-    render(bufnr, items)
+        local _, ret = pcall(handler, bufnr, fn.line)
+        table.insert(items, { text = ret, line = fn.line })
+      end
+      render(bufnr, items)
+    end)
   end
 end
 
 local function setup(opts)
   local virt_text_callback = create_virt_text_callback(opts)
-  vim.api.nvim_create_autocmd("CursorHold", { callback = virt_text_callback })
+
+  local filetypes = opts.filetypes or { "markdown" }
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = filetypes,
+    callback = function(input)
+      local augroup = vim.api.nvim_create_augroup("VIRT_TEXT_CALC", { clear = true })
+      vim.api.nvim_create_autocmd("CursorHold", {
+        buffer = input.bufnr,
+        callback = virt_text_callback,
+        group = "VIRT_TEXT_CALC",
+      })
+    end,
+  })
 end
 
 return {
